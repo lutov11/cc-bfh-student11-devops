a8929a31fa066b30769c555c31913a9876be9c47
Valeriia Lutoshkyna <valeriia.lutoshkyna@students.bfh.ch>
2025-05-10 16:49:54 +0200

    Test pipeline

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "student_id" {
+  description = "Your student ID"
+  type        = string
+}
+
+variable "profile" {
+  description = "AWS CLI profile"
+  type        = string
+  default     = "student11-labaccess"
+}
